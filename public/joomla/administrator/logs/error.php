8a9af4f81b3d4399d242d098112e140287a78927
ErikCronin <strandeddata@gmail.com>
2020-03-07 23:24:05 +1000

    initial commit

---
+++ public/joomla/administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-03-07 04:26:33 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-03-07T04:26:33+00:00	INFO 192.168.33.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-03-07T04:26:42+00:00	INFO 192.168.33.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-03-07T12:45:13+00:00	INFO 192.168.33.1	joomlafailure	Username and password do not match or you do not have an account yet.
